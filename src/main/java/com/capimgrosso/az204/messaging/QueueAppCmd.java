357d588f6ce311b2ab454d6262234ae36d851179
Francisco Filho <francisco@capimgrosso.com>
2023-12-26 21:17:30 -0300

    First commit

---
+++ src/main/java/com/capimgrosso/az204/messaging/QueueAppCmd.java
@@ -0,0 +1,33 @@
+package com.capimgrosso.az204.messaging;
+
+import com.azure.core.http.rest.PagedIterable;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.storage.queue.QueueClientBuilder;
+import com.azure.storage.queue.models.QueueMessageItem;
+
+import java.util.logging.Logger;
+
+public class QueueAppCmd {
+    private static final Logger LOG = Logger.getLogger(QueueAppCmd.class.getSimpleName());
+    public static void main(String[] args) {
+        var url = "https://az204cg001.queue.core.windows.net/taskqueue";
+        var credential = new DefaultAzureCredentialBuilder().build();
+
+        var queueClient = new QueueClientBuilder()
+                .credential(credential)
+                .endpoint(url)
+                .buildClient();
+
+        queueClient.createIfNotExists();
+
+        queueClient.sendMessage("hello queue's");
+        queueClient.sendMessage("I was here");
+
+        PagedIterable<QueueMessageItem> messages =  queueClient.receiveMessages(10);
+        LOG.info("Receiving messages");
+        for (var msg : messages) {
+            LOG.info(msg.getBody().toString());
+            queueClient.deleteMessage(msg.getMessageId(), msg.getPopReceipt());
+        }
+    }
+}
---
+++ src/test/java/com/capimgrosso/az204/messaging/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package com.capimgrosso.az204.messaging;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
